5fcdf6d03286afe04dce52efee6fa39f844aa1d8
Felice Lombardi <felice.lombardi@avanade.com>
2023-05-23 17:22:00 +0200

    funzionante e deployato <3

--- src/components/HelloWorld.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export interface IHelloWorldProps {
-  name: string;
-}
-
-export const HelloWorld: React.FunctionComponent<IHelloWorldProps> = ({ name }) => {
-  return <div>Hello, {name}!</div>;
-};
